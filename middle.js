efc0b81d4b258393818fed84c9e8edf098d8dd31
Humaira Tasnim <hxtasnim@gmail.com>
2022-11-19 02:29:34 +0000

    added middle function

---
+++ middle.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function(array) {
+  let output = [];
+  let middle = Math.floor(array.length / 2);
+  if (array.length > 2) {
+    if (array.length % 2 !== 0) {
+      output.push(array[middle]);
+    } else {
+      output.push(array[middle - 1]);
+      output.push(array[middle]);
+    }
+  }
+  return output;
+};
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
