729c078f9d9144150abc1118462066053e7a465a
Humaira Tasnim <hxtasnim@gmail.com>
2022-11-22 14:57:27 +0000

    added countLetters function

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const results = {};
+  for (const char of str.split(' ').join('')) {
+    if (results[char]) {
+      results[char] += 1;
+    } else {
+      results[char] = 1;
+    }
+  }
+  return results;
+};
+
+const result = countLetters("lighthouse in the house");
+
+assertEqual(result["l"], 1);
+assertEqual(result["h"], 4);
+assertEqual(result["a"], undefined);
+assertEqual(result["b"], undefined);
--- countOnly.js
+++ countOnly.js
@@ -12,7 +12,7 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
-    if (itemsToCount[item]) { 
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
@@ -21,7 +21,7 @@ const countOnly = function(allItems, itemsToCount) {
     }
   }
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
