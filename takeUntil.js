8c84a084b58944e1c7be27ba5e340e443a1daea7
Humaira Tasnim <hxtasnim@gmail.com>
2022-11-24 18:21:30 +0000

    updated takeUntil function

--- takeUntil.js
+++ takeUntil.js
@@ -2,11 +2,12 @@ const takeUntil = function(array, callback) {
   const results = [];
   for (let item of array) {
     if (callback(item)) {
-      return results;
+      break;
     } else {
       results.push(item);
     }
   }
+  return results;
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
