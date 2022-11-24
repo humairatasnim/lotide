e102cea30095232f7df9831566b4330e088ec8d9
Humaira Tasnim <hxtasnim@gmail.com>
2022-11-24 18:35:21 +0000

    added findKey function

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  for (const item of Object.keys(obj)) {
+    if (callback(obj[item])) {
+      return item;
+    }
+  }
+};
+
+let data = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+assertEqual(findKey(data, x => x.stars === 2), "noma");
+assertEqual(findKey(data, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(data, x => x.stars === 4), undefined);
