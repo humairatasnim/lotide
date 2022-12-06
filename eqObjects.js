a73f765d8b49dfd71778c001faf2ba62beda179f
Humaira Tasnim <hxtasnim@gmail.com>
2022-12-06 13:40:01 -0500

    refactored eqObjects function logic

--- eqObjects.js
+++ eqObjects.js
@@ -3,16 +3,16 @@ const eqArrays = require('./eqArrays');
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  } else {
-    for (let key of Object.keys(object1)) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        return eqArrays(object1[key], object2[key]);
-      } else if (object1[key] !== object2[key]) {
-        return false;
-      }
-    }
-    return true;
   }
+  for (let key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    }
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = eqObjects;
