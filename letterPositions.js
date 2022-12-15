5ba0e0f8e6a75371c3a7b4d72c8790caf81ac247
Humaira Tasnim <hxtasnim@gmail.com>
2022-12-15 13:49:06 -0500

    changed array to positionsArray

--- letterPositions.js
+++ letterPositions.js
@@ -2,11 +2,11 @@ const letterPositions = function(sentence) {
   const results = {};
 
   for (let i = 0; i < sentence.length; i++) {
-    let array = [i];
+    let positionsArray = [i];
     if (results[sentence[i]]) {
       results[sentence[i]].push(i);
     } else {
-      results[sentence[i]] = array;
+      results[sentence[i]] = positionsArray;
     }
   }
   return results;
